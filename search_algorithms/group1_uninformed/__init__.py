71fe422579d7eef9ff5d0c2431f2ca18048917dc
TranNhuHung-code <hungtrolldavinl@gmail.com>
2026-06-27 22:28:45 +0700

    Delete search_algorithms directory

--- search_algorithms/group1_uninformed/__init__.py
+++
@@ -1,2 +0,0 @@
-# Nhóm 1: Uninformed Search Algorithms
-from search_algorithms.group1_uninformed.algorithms import bfs, dfs, ids
--- search_algorithms/group2_informed/__init__.py
+++
@@ -1,2 +0,0 @@
-# Nhóm 2: Informed Search Algorithms
-from search_algorithms.group2_informed.algorithms import greedy_best_first, astar, ida_star
